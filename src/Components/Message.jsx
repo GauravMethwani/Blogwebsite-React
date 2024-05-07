8cfbeda126bf012e90627d6be9859b77f05c7dac
gaurav <gmethwani365@gmail.com>
2024-05-07 22:15:00 +0530

    new changes

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,12 +1,10 @@
-
-
 // eslint-disable-next-line react/prop-types
 const Message = ({ isOpen, message, onClose }) => {
     if (!isOpen) return null;
 
     return (
         <div className="fixed z-10 inset-0 overflow-y-auto">
-            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                 <div className="fixed inset-0 transition-opacity" aria-hidden="true">
                     <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
                 </div>
